c46f6d3500844bcff71a86ef9ff369edc29c023f
lemus <lemus777@gmail.com>
2022-11-01 14:04:57 +0100

    instalacion y configuracion webpack y react

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Bienvenido a mi morada. Entre libremente, por su propia voluntad, y deje parte de la felicidad que trae.</h1>
+  );
+}
+
+export default App;
