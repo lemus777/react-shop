87535aafea20ac5f4af730f4fb880c65cdbe8aaf
lemus <lemus777@gmail.com>
2022-11-04 21:11:17 +0100

    estructura terminada, alias, imagenes, notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import '@styles/NotFound.scss';
 
 const NotFound = () => {
   return (
-    <div>
-      <p>No encontrado, error 404.</p>
+    <div className='not-found'>
+      <img src="https://images.pexels.com/photos/3755755/pexels-photo-3755755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="notFound" />
+      <p className='error'>Error 404</p>
+      <p>Lo sentimos, la página a la que intenta acceder no ha sido encontrada.</p>
+      <button className='primary-button navigate-button'>Volver a página principal</button>
     </div>
   );
 }
